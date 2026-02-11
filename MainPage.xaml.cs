e402251cb14f16a2fb50dc3a71e5ef81c549d9ad
Alejandro M. Tineo Moreta <115231713+Alejandrotineo2020@users.noreply.github.com>
2026-02-11 14:18:44 -0400

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace variable; 
+
+public partial class MainPage : ContentPage
+{
+    int contador = 0;
+    const double PI = 3.1416;
+    readonly string name = "Variables Locales";
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnIncrementarClicked(object? sender, EventArgs e)
+    {
+        contador++;
+        lblcontador.Text = contador.ToString();
+    }
+
+    private void OnMostrarVariableClicked(object? sender, EventArgs e)
+    {
+        int edad = 20;
+        string nombre = "Alejandro M. Tineo";
+        string profe = "Juancito";
+
+        lblresultado.Text = $"{name}\n" +
+                            $"PI: {PI}\n" +
+                            $"Usuario: {nombre}\n" +
+                            $"Edad: {edad}\n" +
+                            $"Profesor: {profe}";
+    }
+}
